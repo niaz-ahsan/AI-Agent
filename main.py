874b67548431c1f481f653d517e3f9b3e2a42753
Niaz <thatisniaz@gmail.com>
2026-04-22 08:40:05 -0500

    1. gemini api is integrated and call is made from main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("No API Key found!")
+    client = genai.Client(api_key=api_key)
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        )
+        print(response.text)
+    except:
+        print("Something is wrong with Gemini API")
+
+
+if __name__ == "__main__":
+    main()
