e83ba5505f44cfa8a213e809d0e58f8451f56a94
Niaz <thatisniaz@gmail.com>
2026-04-27 18:53:35 -0500

    11. Prompt is modified to let LLM seek the problem, fix, execute to test the codebase.

--- prompts.py
+++ prompts.py
@@ -9,4 +9,8 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Execute Python scripts
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+When you find out a problem or a bug, try to track it in the codebase.
+Once bug is found, try to fix it in the file. 
+Once fix is done, execute the code and confirm that the solution works.
 """
